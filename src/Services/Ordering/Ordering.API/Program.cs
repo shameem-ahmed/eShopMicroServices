a42172e89c76c5db4a59bad98ef5dfb46f9264f1
Shameem Ahmed <shameem.net1@gmail.com>
2024-08-17 02:10:28 +0530

    completed Ordering.Domain

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the DI container
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,35 @@
+﻿namespace Ordering.Domain.ValueObjects;
+public record Address
+{
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    public string? EmailAddress { get; } = default!;
+    public string AddressLine { get; } = default!;
+    public string Country { get; } = default!;
+    public string State { get; } = default!;
+    public string City { get; } = default!;
+
+    protected Address() 
+    {
+    }
+
+    private Address(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string city)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        AddressLine = addressLine;
+        Country = country;
+        State = state;
+        City = city;
+    }
+
+    public static Address Of(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string city)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+        return new Address(firstName, lastName, emailAddress, addressLine, country, state, city);
+    }
+}
+ 
